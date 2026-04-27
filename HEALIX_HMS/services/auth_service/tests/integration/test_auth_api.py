ea19c16dcde2d076b78b1b029751fce68a5115fb
Thamizh-S24 <thamizharasansakthivel2025@gmail.com>
2026-04-27 11:59:23 +0000

    updated jenkinsfile config for integration test

---
+++ HEALIX_HMS/services/auth_service/tests/integration/test_auth_api.py
@@ -0,0 +1,12 @@
+import requests
+
+BASE_URL = "http://localhost:8000"
+
+def test_docs_endpoint():
+    response = requests.get(f"{BASE_URL}/docs")
+    assert response.status_code == 200
+
+
+def test_health_check():
+    response = requests.get(f"{BASE_URL}/docs")
+    assert response.status_code == 200
