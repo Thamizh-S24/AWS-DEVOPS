90768f5890a284698eba4115716ffc22b11f1b15
Thamizh-S24 <thamizharasansakthivel2025@gmail.com>
2026-04-30 08:12:13 +0000

    fixed conftest imports for all services

--- HEALIX_HMS/services/auth_service/tests/conftest.py
+++ HEALIX_HMS/services/auth_service/tests/conftest.py
@@ -1,6 +1,15 @@
 import pytest
 from fastapi.testclient import TestClient
+import sys
+import os
+
+# Dynamically add service root to path
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if BASE_DIR not in sys.path:
+    sys.path.append(BASE_DIR)
+
 from app.main import app
+
 
 @pytest.fixture(scope="module")
 def client():
